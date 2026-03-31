bbdad055fc86cd4eb885fd6284ff4ca95f3f586d
Sofia <sofiabtorquato@gmail.com>
2026-03-31 00:00:17 -0300

    integração api

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,13 @@
-const baseApi = 'https://rickandmortyapi.com/';
+export async function buscarFotos(){
+    try{
+        const urlApi = await fetch('https://rickandmortyapi.com/api/character')
+        const dados = await urlApi.json();
+        return dados.results;
+
+    }
+    catch(error){
+        console.error('Erro ao capturar imagem');
+        return [];
+    }
+}
 
